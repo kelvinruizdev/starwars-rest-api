6c3c59bb4d5e1c3852c9682887b853d3e8d21b2e
Kelvin Ruiz <kelvinruiz.developer@gmail.com>
2023-08-01 15:42:42 -0800

    ADD: Models, routes Peoples and Planets

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,82 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+#PEOPLE
+@app.route('/people')
+def get_all_people():
+    all_people = People.query.all()
+    return jsonify(list(map(lambda item: item.serialize(), all_people))), 200
+
+@app.route('/people/<int:people_id>', methods = ['GET'])
+def get_people_id(people_id = None):
+
+    person = People.query.filter_by(id = people_id).one_or_none()
+    if person is None:
+        return jsonify({"message" : "No coincidence"}), 400
+
+    return jsonify(person.serialize()), 200
+
+@app.route('/people', methods = ['POST'])
+def add_people():
+    data = request.json
+    
+    if data is None or data is {}:
+        return jsonify({"message" : "data is empty"}), 400
+
+    people = People()
+    people.name = data.get('name', None)
+    people.gender = data.get('gender', None)
+    people.skin_color = data.get('skin_color', None)
+    people.birth_year = data.get('birth_year', None)
+
+    db.session.add(people)
+
+    try:
+        db.session.commit()
+    except Exception as err:
+        db.session.rollback()
+        return jsonify({'message': err.args}), 500    
+
+    return jsonify({"message" : "People correctly added"}), 200    
+
+# PLANET
+@app.route('/planet')
+def get_all_planet():
+    all_planets = Planet.query.all()
+    return jsonify(list(map(lambda item: item.serialize(), all_planets))), 200
+
+@app.route('/planet/<int:planet_id>', methods = ['GET'])
+def get_planet(planet_id = None):
+
+    planet = Planet.query.filter_by(id = planet_id).one_or_none()
+    if planet is None:
+        return jsonify({"message" : "No coincidence"}), 400
+
+    return jsonify(planet.serialize()), 200
+
+@app.route('/planet', methods = ['POST'])
+def add_planet():
+    data = request.json
+    
+    if data is None or data is {}:
+        return jsonify({"message" : "data is empty"}), 400
+
+    planet = Planet()
+    planet.name = data.get('name', None)
+    planet.climate = data.get('climate', None)
+    planet.terrain = data.get('terrain', None)
+    planet.population = data.get('population', None)
+
+    db.session.add(planet)
+
+    try:
+        db.session.commit()
+    except Exception as err:
+        db.session.rollback()
+        return jsonify({'message': err.args}), 500    
+
+    return jsonify({"message" : "Planet correctly added"}), 200    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,41 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    gender = db.Column(db.String(20), nullable=False)
+    birth_year = db.Column(db.String(50), nullable=False)
+    skin_color = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<People {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "birth_year": self.birth_year,
+            "skin_color": self.skin_color
+        }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    climate = db.Column(db.String(50), nullable=False)
+    terrain = db.Column(db.String(50), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Planet {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population
+        }    
